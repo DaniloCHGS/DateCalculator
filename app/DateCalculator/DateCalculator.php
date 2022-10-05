205f885bf8ca725d4744a546e505df4aac0a9bec
DaniloCHGSDev <danilobm500@gmail.com>
2022-10-05 16:02:15 -0300

    Ajustes

--- app/DateCalculator/DateCalculator.php
+++ app/DateCalculator/DateCalculator.php
@@ -14,43 +14,39 @@ class DateCalculator
     /**
      * Time Zone
      */
-    public static $time_zone = 'America/Sao_Paulo';
+    private static $time_zone = 'America/Sao_Paulo';
     /**
      * Ajusta Time Zone
      * @param string $time_zone Time Zone definida || America/Sao_Paulo
-     * @return void
      */
-    public static function setTimeZone($time_zone = null)
+    public static function setTimeZone($time_zone = null): void
     {
         self::$time_zone = $time_zone ?? self::$time_zone;
     }
     /**
      * Retorna uma data entre um intervalo de dias
      * @param integer $interval Intervalo a partir da data escolhida
-     * @param string $date Data base para calcular o dia. || Data atual
-     * @return string Intervalo de datas já calculado
+     * @param string $date Data base para calcular o dia || Data atual
      */
-    public static function getIntervalDay($interval = 1, $date = null)
+    public static function getIntervalDay($interval = 1, $date = null): string
     {
         return self::getDateInterval($interval, $date, "D");
     }
     /**
      * Retorna uma data entre um intervalo de mêses
      * @param integer $interval Intervalo a partir da data escolhida
-     * @param string $date Data base para calcular o mês. || Data atual
-     * @return string Intervalo de datas já calculado
+     * @param string $date Data base para calcular o mês || Data atual
      */
-    public static function getIntervalMonth($interval = 1, $date = null)
+    public static function getIntervalMonth($interval = 1, $date = null): string
     {
         return self::getDateInterval($interval, $date, "M");
     }
     /**
      * Retorna uma data entre um intervalo de anos
      * @param integer $interval Intervalo a partir da data escolhida
-     * @param string $date Data base para calcular o ano. || Data atual
-     * @return string Intervalo de datas já calculado
+     * @param string $date Data base para calcular o ano || Data atual
      */
-    public static function getIntervalYear($interval = 1, $date = null)
+    public static function getIntervalYear($interval = 1, $date = null): string
     {
         return self::getDateInterval($interval, $date, "Y");
     }
@@ -58,10 +54,8 @@ class DateCalculator
      * Retorna uma data entre um intervalo
      * @param integer $interval Intervalo a partir da data escolhida
      * @param string $date Data base para calcular o intervalo || Data atual
-     * @param string $type Tipo de data escolhida. D = dia, M = mês, Y = ano. Vazio para dia
-     * @return string Intervalo de datas já calculado
      */
-    public static function getDateInterval($interval = 1, $date = null, $type = "D")
+    public static function getDateInterval($interval = 1, $date = null, $type = "D"): string
     {
         //Ajusta do Time zone de onde buscar a data
         date_default_timezone_set(self::$time_zone);
@@ -72,17 +66,19 @@ class DateCalculator
         //Instancia a classe DateTime com a Time Zone definida com a data inicial
         $dateTime = new DateTime($initial, new DateTimeZone(self::$time_zone));
 
+        //Recebe o intervalo como número inteiro
+        $interval = floor($interval) > 0 ? floor($interval) : ceil($interval);
+
         //Recebe a data
-        $newDate = $dateTime->add(new DateInterval("P" . floor($interval) . self::setType($type)));
+        $newDate = $dateTime->add(new DateInterval("P" . $interval . self::setType($type)));
 
         return $newDate->format('Y-m-d');
     }
     /**
      * Trativa do tipo de data selecionada
      * @param string $type
-     * @return string
      */
-    private static function setType($type)
+    private static function setType($type): string
     {
         $selected = strtoupper($type);
 
--- index.php
+++ index.php
@@ -5,15 +5,15 @@ require __DIR__ . "/vendor/autoload.php";
 use App\DateCalculator\DateCalculator;
 
 //Retorna 30 dias da data atual
-$newDay = DateCalculator::getIntervalDay(1, null);
+$day = DateCalculator::getIntervalDay(30, null);
 
 //Retorna 1 mês da data atual
-$nextMonth = DateCalculator::getIntervalMonth(1);
+$month = DateCalculator::getIntervalMonth(1);
 
 //Retorna 10 anos da data atual
-$newYear = DateCalculator::getIntervalYear(10);
+$year = DateCalculator::getIntervalYear(10);
 
 //Retorna 6 meses da data atual
-$newDate = DateCalculator::getDateInterval(6, null, "M");
+$date = DateCalculator::getDateInterval(6, null, "M");
 
-echo $newDay;
+echo $day;
